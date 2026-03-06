1780bdd88e45df8b2ee7c0e866b47defe0d74284
mrdalka6-del <mrdalka6@gmail.com>
2026-03-06 14:02:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 
+async function upload(){
+
+let file=document.getElementById("video").files[0]
+
+if(!file){
+alert("Video geli")
+return
+}
+
+document.getElementById("status").innerText="AI ayaa shaqeynaya..."
+
+let form=new FormData()
+form.append("video",file)
+
+let res=await fetch("/dub",{method:"POST",body:form})
+
+let data=await res.json()
+
+document.getElementById("status").innerText="Dubbing waa diyaar"
+
+window.open(data.video)
+
+}
