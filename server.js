7609a0163ac8d4ac3b6979c470d6f092843e103d
mrdalka6-del <mrdalka6@gmail.com>
2026-03-06 14:03:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
 
+const express=require("express")
+const multer=require("multer")
+
+const app=express()
+const upload=multer({dest:"uploads/"})
+
+app.post("/dub",upload.single("video"),async(req,res)=>{
+
+// halkan AI dubbing ayaa ka dhici lahaa
+
+res.json({
+video:"dubbed-video.mp4"
+})
+
+})
+
+app.listen(3000,()=>{
+
+console.log("server running")
+
+})
